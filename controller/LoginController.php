6158e90a39e17095119199e2f3d98da8d1d0516b
Daniel Eduardo Macias Estrada <danma1708@gmail.com>
2023-04-10 08:37:35 -0600

    Update LoginController.php

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,47 +1,53 @@
 <?php
 
-include "model/config.php";
+// Clase LoginController: Se encarga de la comunicación entre el modelo del usuario y las vistas y rutas relacionadas con el inicio de sesión
 
 class LoginController {
+    // Ruta relativa hacía la vista de login
     private $viewLogin = "view/login.php";
+    // Ruta relativa hacía el modelo del usuario
     private $modelUser = "model/UserModel.php";
 
-
+    // Método que carga la vista de login
     public function loadPage(){
         $page = "Log in";
         include ($this->viewLogin);
     }
-
+    
+    // Método que destruye todas las variables de la sesión y la sesión actual
     public function logOut($popup){
         session_start();
         $_SESSION = array();
         session_destroy();
         $this->loadPage();
     }
-
+    
+    // Método que carga el modelo de usuarios
     private function requireModelUser(){
         require_once $this->modelUser;
     }
-
+    
+    //Método para checar si los datos ingresados en le formulario de inicio de sesión son los correctos
     public function check(){
         if(isset($_POST['user']) && isset($_POST['pass'])){
-            session_start();
-            $this->requireModelUser();
+            session_start(); // Se inicia la sesión
+            $this->requireModelUser(); // Se llama a los métodos del modelo de usuarios
         
             $username =  $_POST['user'];
-            $password =  hash('sha256',$_POST['pass']);
-            $filterCredential = array('username'=>$username,'password'=>$password);
-
-            $result = getUsers($filterCredential);
-        
+            $password =  hash('sha256',$_POST['pass']); // La contraseña está encriptada en la base de datos, por lo que se convierte para realizar comparaciones
+            $filterCredential = array('username'=>$username,'password'=>$password); // Se reune las credenciales por medio de un array 
+            
+            // Se solicita la información del usuario para verificar si existe
+            $result = getUsers($filterCredential); 
+            
+            // si se trae al menos una consulta, se guarda los datos en una variable SESSION
             if(count($result) == 1){
                 $_SESSION['id'] = $result[0]['id'];
                 $_SESSION['username'] = $result[0]['username'];
                 $_SESSION['password'] = $result[0]['password'];
                 $_SESSION['type'] = $result[0]['type'];
                 
-                //var_dump($_REQUEST);
-                
+                // Para la respuesta a ajax, se indica si la solicitud fue exitosa o no, y en caso de serlo, se envia hacia el dashboard
                 return json_encode(array('exito' => true, 'url' => 'index.php?controller=StoreController&action=loadPage'));
             } else {
                 return json_encode(array('exito' => false));
