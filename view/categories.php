009be34f5a3056a7c069466a166558bcde86e138
Daniel Eduardo Macias Estrada <danma1708@gmail.com>
2023-04-19 07:06:38 -0600

    Update categories.php

--- view/categories.php
+++ view/categories.php
@@ -35,6 +35,17 @@
                 </div>
               </div>
               
+              <!-- Toast  -->
+              <div class="bs-toast toast toast-placement-ex m-2 fade bg-success top-0 end-0 hide" id="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+                <div class="toast-header">
+                  <i class="bx bx-bell me-2"></i>
+                  <div class="me-auto fw-semibold" id="toastHeader"></div>
+                  <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body" id="toastBody"> </div>
+              </div>
+              <!-- / Toast  -->
+              
 
               <table id="example" class="display table-responsive text-nowrap" style="width:100%">
                 <thead>
